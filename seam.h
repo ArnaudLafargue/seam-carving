2ced6c2bf59e6e770b42d8d4e4256092d125e3a1
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2018-05-14 14:39:54 +0200

    Add files via upload

--- seam.h
+++ seam.h
@@ -6,12 +6,18 @@ using namespace Imagine;
 #include <vector>
 #pragma once
 
-std::vector<Coords<2> > Bellman_vertical(Image<byte> &E,int &last_state);
+std::vector<Coords<2> > Bellman_vertical(Image<byte> E, int &seam_value);
+std::vector<Coords<2> > Bellman_horizontal(Image<byte> E, int &seam_value);
+
 void dessine (std::vector<Coords<2>> seam);
 int min (int a , int b , int c);
-Image<Color> assemble_vertical(Image<Color> I,std::vector<Coords<2>> seam);
-Image<Color> remove_vertical(Image<Color> I, Image<byte> Vx, Image<byte> Vy, int k);
+
+Image<Color> assemble_vertical(Image<Color> I, std::vector<Coords<2>> seam);
+Image<Color> assemble_horizontal(Image<Color> I, std::vector<Coords<2>> seam);
+
 Image<Color> Transpose(Image<Color> I);
+
+Image<Color> remove(Image<Color> I,int w, int h);
 
 
 #endif // SEAM_H
